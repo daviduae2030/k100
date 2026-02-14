e28a7b52b7182be2b771fa13a51726fe4570468d
daviduae2030 <retajdwc@gmail.com>
2026-02-14 16:25:29 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,71 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-002",
+      "name": "002",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 375,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.08314913069553143,
+          "pitch": 0.3054028294480027,
+          "rotation": 0,
+          "target": "1-003"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-003",
+      "name": "003",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5542999955340218,
+          "pitch": 0.23301330756269323,
+          "rotation": 0,
+          "target": "0-002"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "k1000",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
